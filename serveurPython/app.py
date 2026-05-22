80f60770b5286691dd2ae9018c68ef4efba6e181
simeon-issanga <simeonissanga33@gmail.com>
2026-05-22 16:00:41 +0000

    test IA React

--- serveurPython/app.py
+++ serveurPython/app.py
@@ -7,13 +7,31 @@ app = Flask(__name__)
 
 client = OpenAI(
     api_key=os.environ.get("API_KEY_DEEPSEEK"),
-    base_url="https://api.deepseek.com/v1"
+    base_url="http://10.3.17.220:1234/v1"
 )
 
 @app.route('/api/compile', methods=['POST'])
 def compile_code():
-    ## code 
-    return jsonify({"message": "API prête !"})
+    try :
+        donnes = request.get_json()
+        code_c = donnes.get('code', '')
+    
+    if not code_utilisateur.strip():
+        return jsonify({"status": "error", "message": "Le code est vide !"})
+    
+    question = f"Voici un code C :\n{code_c}\nPeux-tu me dire si ce code est correct et s'il peut être compilé ?"
+
+    reponse = client.chat.completions.create(
+        #model="gpt-3.5-turbo",
+        model="local-model",
+        messages=[
+            {"role": "user", "content": question}],
+        temperature=0.2
+
+    )
+    texte_reponse = reponse.choices[0].message.content
+    
+    return jsonify({"status": "success", "response": texte_reponse})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- site/src/App.jsx
+++ site/src/App.jsx
@@ -2,14 +2,46 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
+  const [codeC, setCodeC] = useState('')
+  const [reponseIA, setReponseIA] = useState('')
+  
+  const envoyer = async() => {
+    try {
+      const reponse = await fetch('/api/compile', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ code: codeC })
+        })
 
+      const donnees = await reponse.json()
+      
+      if (donnees.status === 'success') {
+        setReponseIA(donnees.message)
+      } else {
+        setReponseIA("Erreur du serveur : " + donnees.message)
+      }
+    
+    }catch (error) {
+      console.error('Erreur lors de l\'envoi du code :', error)
+    }
+  }
+  
   return (
     <>
+    
       <h1>Ceci est une page de fou</h1>
       <div class="container" >
         <Encadre langage="C"/>
         <Encadre langage="IR"/>
       </div>
+      <textarea 
+            value={codeC}
+            onChange={(e) => setCodeC(e.target.value)}
+            placeholder="Entrez votre code C ici"
+      />
+      <button onClick={envoyer}>Envoyer</button>
+           
+      <div>{reponseIA}</div>
     </>
   )
 }
