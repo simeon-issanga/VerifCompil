a918a9b36d88f534fb09ba2799fe3c5cede2ecf9
simeon-issanga <simeonissanga33@gmail.com>
2026-05-29 10:24:15 +0200

    les tests

---
+++ tests/testClang.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def test_clang_works():
+    test_code = "int main() { return 0; }"
+    with open("test.c", "w") as f:
+        f.write(test_code)
+    
+    result = subprocess.run(["clang", "test.c", "-emit-llvm", "-S", "-o", "test.ll"])
+    assert result.returncode == 0
+    print("Test Clang : OK")
+
+if __name__ == "__main__":
+    test_clang_works()
