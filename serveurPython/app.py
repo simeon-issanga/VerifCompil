470583df7736935a2dce9f5847f536b90a7c8c8b
simeon-issanga <simeonissanga33@gmail.com>
2026-06-01 11:52:49 +0200

    change model

--- serveurPython/app.py
+++ serveurPython/app.py
@@ -146,7 +146,7 @@ def compile_code():
             }
         else :
             reponse = client.chat.completions.create(
-                model="llama-3.3-70b-versatile",
+                model="groq/compound",
                 messages=[
                     {"role": "system", "content": prompt_sys},
                     {"role": "user", "content": f"Voici le code C :\n{code_c}\nVoici le code LLVM IR :\n{llvm_ir}"}
