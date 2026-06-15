6a7880d276517c068885107a2c33166f5789caaf
Lucas Oustaloup <oustaloup.lucas@gmail.com>
2026-06-15 15:12:24 +0200

    ajout de l'affichage du temps d'execution pour chaque pass et mise à jour des langues pour les mots non encore traduits

--- site/src/i18n.js
+++ site/src/i18n.js
@@ -10,32 +10,47 @@ i18n
         resources: {
             en: {
                 translation: {
-                    "app-title": "C to LLVM - IR",
+                    "app-title": "parallele React EN",
                     "title": "Translation from C to LLVM - IR",
                     "C_code": "C code",
                     "IR_code": "IR code (before optimisation)",
                     "submit" : "submit",
-                    "code_version":"code version n°"
+                    "code_version":"code version n°",
+                    "inexecutable":"unachievable",
+                    "fileChoice": "import a C/C++ file",
+                    "allPasses":"all passes",
+                    "modifPasses":"modification passes",
+                    "modification":"modification"
                 }
             },
             fr: {
                 translation: {
-                    "app-title": "C vers LLVM - IR",
+                    "app-title": "parallele React FR",
                     "title": "Traduction de C vers LLVM - IR",
                     "C_code": "code en C",
                     "IR_code": "code en IR (avant optimisation)",
                     "submit" : "valider",
-                    "code_version":"version du code n°"
+                    "code_version":"version du code n°",
+                    "inexecutable":"inexécutable",
+                    "fileChoice":"Choisir un fichier C / C++",
+                    "allPasses":"toutes les passes",
+                    "modifPasses":"passes de modification",
+                    "modification":"modification"
                 }
             },
             es: {
                 translation: {
-                    "app-title": "de C a LLVM - IR ",
+                    "app-title": "parallele React ES",
                     "title": "Traducción de C a LLVM-IR",
                     "C_code": "código en C",
                     "IR_code": "código en IR (antes de la optimización)",
                     "submit" : "confirmar",
-                    "code_version":"versión del código n°"
+                    "code_version":"versión del código n°",
+                    "inexecutable":"inaplicable",
+                    "fileChoice":"importar un fichero C / C++",
+                    "allPasses":"todos los passes",
+                    "modifPasses":"passes de modificación",
+                    "modification":"modificación"
                 }
             }
         }
