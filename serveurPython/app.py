fc8bc5ea55de87c164978100a94e9770c76a5949
simeon-issanga <simeonissanga33@gmail.com>
2026-06-30 16:09:59 +0200

    changement de modele (deepseek)

--- serveurPython/app.py
+++ serveurPython/app.py
@@ -11,9 +11,9 @@ from db import insert_prompt
 app = Flask(__name__)
 
 client = ollama.Client(host='http://ollama:11434')
-MODEL="mistral-small"
-#MODEL == "deepseek-r1:14b"
-#MODEL == "qwen2.5-coder:7b"
+#MODEL="mistral-small"
+MODEL = "deepseek-r1:14b"
+#MODEL = "qwen2.5-coder:7b"
 idM=0
 ############# main #############
 
